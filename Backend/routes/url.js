3d3ff788adf04a8c7e9d8ae9f79b3b49a7ed1672
Manikanta420m <manikanta420m@gmail.com>
2026-09-02 11:12:53 +0530

    "Ready to Deploy"

--- Backend/routes/url.js
+++ Backend/routes/url.js
@@ -6,7 +6,36 @@ const router = express.Router();
 
 router.post("/shorten",async(req,res)=>{
    try{
-     const {originalUrl} = req.body
+     const {originalUrl} = req.body;
+
+     if(!originalUrl){
+        return res.status(400).json({
+           error : "Original URL is required"
+        });
+     }
+
+     try{
+       new URL(originalUrl);
+     }
+     catch(e){
+        return res.status(400).json({
+           error : "Invalid URL"
+        });
+     }
+     let shortId;
+     let exists = true;
+     while(exists){
+      shortId=nanoid(7);
+      exists = await Url.findOne({shortId});
+     }
+
+     const url = await Url.create({
+        shortId,originalUrl
+     });
+     res.json({
+       shortId : url.shortId,
+       shortUrl : `${process.env.BASE_URL}/${url.shortId}`
+     });
    }
    catch(e){
       console.log(e);
@@ -15,3 +44,24 @@ router.post("/shorten",async(req,res)=>{
       });
    }
 });
+
+router.get("/:shortId",async (req,res)=>{
+   try{
+       const {shortId} = req.params;
+       const url = await Url.findOne({shortId});
+       if(!url){
+         return res.status(404).json({error : "URL not found"});
+       }
+       url.clicks+=1;
+       await url.save();
+       return res.redirect(url.originalUrl);
+   }
+   catch(e){
+      console.log(e);
+      res.status(500).json({
+         error : "Server Error"
+      });
+   }
+});
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -2,20 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import urlRoutes from './routes/url.js';
 
 dotenv.config()
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors({
   origin : process.env.FRONTEND_URL,
   methods : ['POST','GET'] 
 }));
 
-app.get("/",(req,res)=>{
-   res.send("Hello World");
-});
+app.use('/',urlRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
